796116ba9dfb19b9ce07dc167cdd176d21e11c39
Abhi <mndabhinavsai@gmail.com>
2026-03-12 15:13:34 +0530

    ui: use icon-only edit/delete actions in admin tables

--- admin-page/app.js
+++ admin-page/app.js
@@ -420,8 +420,12 @@
 
   function rowActions(type, id) {
     return '<div class="row-actions">' +
-      '<button class="btn btn-secondary btn-sm" data-action="edit" data-type="' + type + '" data-id="' + id + '">Edit</button>' +
-      '<button class="btn btn-secondary btn-sm danger" data-action="delete" data-type="' + type + '" data-id="' + id + '">Delete</button>' +
+      '<button class="row-icon-btn" type="button" title="Edit" aria-label="Edit" data-action="edit" data-type="' + type + '" data-id="' + id + '">' +
+        '<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zm14.71-9.04a1.003 1.003 0 0 0 0-1.42l-2.5-2.5a1.003 1.003 0 0 0-1.42 0l-1.96 1.96 3.75 3.75 2.13-1.79z"></path></svg>' +
+      '</button>' +
+      '<button class="row-icon-btn danger" type="button" title="Delete" aria-label="Delete" data-action="delete" data-type="' + type + '" data-id="' + id + '">' +
+        '<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M6 7h12l-1 14H7L6 7zm3-3h6l1 2h4v2H4V6h4l1-2z"></path></svg>' +
+      '</button>' +
       '</div>';
   }
 
